e89077eb96f871bcd669d84dbe6b8850ba28c8c3
Maxim <maks.ponkratenkov@mail.ru>
2023-09-05 08:26:35 +0300

    End of section 6

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,44 @@
+import axios, { AxiosResponse } from "axios";
+import { Activity } from "../models/activity";
+import { error } from "console";
+
+const sleep = (delay: number) => {
+    return new Promise((resolse) => {
+        setTimeout(resolse, delay)
+    })
+}
+
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
+axios.interceptors.response.use(async response => {
+    try {
+        await sleep(1000);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return await Promise.reject(error);
+    }
+})
+
+const responceBody = <T> (responce: AxiosResponse<T>) => responce.data;
+
+const requests = {
+    get: <T> (url: string) => axios.get<T>(url).then(responceBody),
+    post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responceBody),
+    put: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responceBody),
+    del: <T> (url: string) => axios.delete<T>(url).then(responceBody),
+}
+
+const Activities = {
+    list: () => requests.get<Activity[]>('/activities'),
+    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+    create: (activity: Activity) => axios.post<void>('/activities', activity),
+    update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => axios.delete<void>(`/activities/${id}`)
+}
+
+const agent = {
+    Activities
+}
+
+export default agent;
